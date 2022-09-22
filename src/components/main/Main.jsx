4eb351962b8061cd1bc88979f599ada43cd3647f
annyxvs <annyjp05@gmail.com>
2022-09-22 08:51:00 -0300

    f

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,7 +6,7 @@ export const Main = () =>{
     return(
         <S.Main>
             <div className='content'>
-                <img src={imgVR} alt="" />
+                <img src={imgVR} alt="interactive men witch VR" />
                 <aside>
                     <h2>The leader in interactive VR</h2>
                     <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
